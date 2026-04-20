2744273bff8fa7c7cdfd5349580b8693a991f919
sesha <sesha@yahoo.com>
2026-04-20 16:09:10 +0200

    Improved HTML and CSS

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,4 @@
 import json
-from idlelib.replace import replace
 
 FILE_PATH = "animals_data.json"
 HTML_FILE_PATH = "animals_template.html"
@@ -9,26 +8,40 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data(FILE_PATH)
-output = ''
-for data in animals_data:
-    try:
-        if (data['name'] and data['characteristics']['diet']
-                and data['locations'][0] and data['characteristics']['type']):
-            output += f'<li class="cards__item">'
-            output += f'<p class="card__text">'
-            output += f'<div class="card__title">{data['name']}</div><br>'
-            output += f"<strong>Diet:</strong> {data['characteristics']['diet']}<br><br>\n"
-            output += f"<strong>Location:</strong> {data['locations'][0]}<br><br>\n"
-            output += f"<strong>Type:</strong> {data['characteristics']['type']}<br>\n"
-            output += f'</p>'
-            output += f'</li>'
-    except KeyError:
-        continue
+def serialize_animal(animal_obj):
+    output = ''
+    output += f'<li class="cards__item">'
+    output += f'<div class="card__title">{animal_obj['name']}</div>'
+    output += f'<div class="card__text">'
+    output += f'<ul class="cards">'
+    output += f'<li class="cards__item"><strong>Diet:</strong> {animal_obj['characteristics']['diet']}</li>\n'
+    output += f'<li class="cards__item"><strong>Location:</strong> {animal_obj['locations'][0]}</li>\n'
+    output += f'<li class="cards__item"><strong>Type:</strong> {animal_obj['characteristics']['type']}</li>\n'
+    output += f'<li class="cards__item"><strong>Life expectancy:</strong> {animal_obj['characteristics']['lifespan']}</li>\n'
+    output += f'</ul>'
+    output += f'</div>'
+    output += f'</li>'
+    return output
 
-with open(HTML_FILE_PATH, "r") as read_file:
-    read_html = read_file.read().replace("__REPLACE_ANIMALS_INFO__",
-                                         output)
 
-with open("animals.html", "w") as write_file:
-    write_file.write(read_html)
+def main():
+    animals_data = load_data(FILE_PATH)
+    output = ''
+    for data in animals_data:
+        try:
+            if (data['name'] and data['characteristics']['diet']
+                    and data['locations'][0] and data['characteristics']['type']):
+                output += serialize_animal(data)
+        except KeyError:
+            continue
+
+    with open(HTML_FILE_PATH, "r") as read_file:
+        read_html = read_file.read().replace("__REPLACE_ANIMALS_INFO__",
+                                             output)
+
+    with open("animals.html", "w") as write_file:
+        write_file.write(read_html)
+
+
+if __name__ == "__main__":
+    main()
